7a449948aefc63ebf35cd1fd1b3ead8810474339
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-18 19:31:56 +0300

    make build

---
+++ index.7a92f92a.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelectorAll(".list-item__link")).forEach(function(e){e.addEventListener("click",function(r){r.preventDefault(),document.querySelector("#largeImg").src=e.href})});
+//# sourceMappingURL=index.7a92f92a.js.map
